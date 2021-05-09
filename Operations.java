0083b87f45f409a6357e90f869b24a545bc57714
DilipKumar5891 <77402769+DilipKumar5891@users.noreply.github.com>
2021-05-09 21:07:07 +0530

    Create Operations.java

---
+++ Operations.java
@@ -0,0 +1,66 @@
+package reusablecode;
+
+import org.testng.Assert;
+
+
+public class Toastmessagevalidation {
+
+//	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+//		public  RGcreatedToast(String toast message) {
+//			
+//			
+//			if (RgcToast.equals("Rate Group created successfully")) {
+//				Assert.assertTrue(true);
+//			}
+//			else {
+//				Assert.assertTrue(false);
+//			}
+//		}
+
+//	}
+
+//	public void RGcreatedToast(String rgcToast) {
+//		// TODO Auto-generated method stub
+//		if (rgcToast.equals("Rate Group created successfully")) {
+//			Assert.assertTrue(true);
+//			
+//		}
+//		else if (rgcToast.equals("Rate Group updated successfully")) {
+//			Assert.assertTrue(true);
+//			
+//		}
+//		else {
+//			Assert.assertTrue(false);
+//		}
+//		
+//	}
+	
+	
+	
+	public void RGcreatedToast(String rgcToast) {
+		// TODO Auto-generated method stub
+		if (rgcToast.equals("Rate Group created successfully")) {
+			Assert.assertTrue(true);
+			
+		}
+		else if (rgcToast.equals("Rate Group updated successfully")) {
+			Assert.assertTrue(true);
+			
+		}
+		else {
+			Assert.assertTrue(false);
+		}
+		
+	}
+	
+	
+	
+	public void pagination() {
+		
+	}
+	
+
+}
